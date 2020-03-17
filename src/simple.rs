50721f11fda16df321319405a1ac5f3c04c4f250
Michał Zieliński <michal@zielinscy.org.pl>
2020-03-17 21:45:24 +0000

    _

--- src/simple.rs
+++ src/simple.rs
@@ -2,64 +2,100 @@ use std::cell::RefCell;
 use std::collections::hash_map::Entry;
 use std::collections::HashMap;
 use std::collections::VecDeque;
-use std::rc::Rc;
+use std::iter::Iterator;
 
 use super::common::*;
 
-trait Listener<T> {
-    fn on_delta(&self, t: &T, delta: i64);
+trait Listener<'a, T> {
+    fn on_delta(&'a self, t: &T, delta: i64);
 }
 
-pub struct System {
-    callback_queue: RefCell<VecDeque<Box<dyn FnOnce() -> ()>>>,
+pub struct System<'a> {
+    callback_queue: RefCell<VecDeque<Box<dyn 'a + FnOnce() -> ()>>>,
+    arena: dynamic_arena::DynamicArena<'a, dynamic_arena::NonSend>,
 }
 
-impl System {
-    pub fn new() -> System {
+impl<'a> System<'a> {
+    pub fn new() -> System<'a> {
         System {
             callback_queue: RefCell::new(VecDeque::new()),
+            arena: dynamic_arena::DynamicArena::new_bounded(),
         }
     }
-}
 
-pub struct Rel<T> {
-    sys: System,
-    listeners: RefCell<Vec<Box<dyn Listener<T>>>>,
-}
-
-impl<T> Rel<T> {
-    fn impl_delta(&self, t: &T, delta: i64) {
-        let f = || {
-            for listener in self.listeners.borrow().iter() {
-                listener.on_delta(t, delta);
-            }
-        };
-        f();
-        //self.sys.callback_queue.borrow_mut().push_back(Box::new(f))
-    }
-
-    fn add_listener(&self, f: Box<dyn Listener<T>>) {
-        self.listeners.borrow_mut().push(f)
+    pub fn alloc<T>(&'a self, a: T) -> &'a T {
+        self.arena.alloc(a)
     }
 }
 
-impl<'a, T> Rel<T> {
-    fn impl_new(sys: System) -> Rc<Rel<T>> {
-        Rc::new(Rel {
+pub fn with_system<'a, T>(f: impl FnOnce(&'a System<'a>) -> T) -> T {
+    let sys: System<'a> = System::new();
+    let sys_ref: &System<'a> = &sys;
+    // the lifetime of sys_ref is *slightly* shorter, but we know that sys_ref_ok will not be used
+    // anyway after [f] returns, so this is safe.
+    let sys_ref_ok: &'a System<'a> = unsafe { std::mem::transmute(&sys_ref) };
+    f(sys_ref_ok)
+}
+
+pub trait RelMeta<'a, T> {
+    fn iter_value(&'a self) -> dyn Iterator<Item = T>;
+}
+
+pub struct RelImpl<'a, T> {
+    sys: &'a System<'a>,
+    listeners: RefCell<Vec<&'a dyn Listener<'a, T>>>,
+}
+
+pub struct Rel<'a, T>(&'a RelImpl<'a, T>);
+
+impl<'a, T: Row> RelImpl<'a, T> {
+    pub fn new(sys: &'a System<'a>) -> &'a RelImpl<'a, T> {
+        sys.arena.alloc(RelImpl {
             sys,
             listeners: RefCell::new(Vec::new()),
         })
     }
+
+    fn make_impl_delta_cb(&'a self, t: T, delta: i64) -> Box<dyn 'a + FnOnce() -> ()> {
+        Box::new(move || {
+            let b = self.listeners.borrow();
+            for listener_box in b.iter() {
+                listener_box.on_delta(&t, delta);
+            }
+        })
+    }
+
+    fn impl_delta(&'a self, t: &T, delta: i64) {
+        let impl_delta_cb = self.make_impl_delta_cb(t.clone(), delta);
+
+        self.sys
+            .callback_queue
+            .borrow_mut()
+            .push_back(impl_delta_cb);
+    }
 }
 
-pub struct DataRel<T: Row> {
-    pub rel: Rc<Rel<T>>,
-    vals: HashMap<T, ()>,
+impl<'a, T: Row> Rel<'a, T> {
+    fn add_listener(&self, f: &'a dyn Listener<'a, T>) {
+        self.0.listeners.borrow_mut().push(f)
+    }
 }
 
-impl<T: Row> DataRel<T> {
-    pub fn add(&mut self, t: &T) {
-        let entry = self.vals.entry(t.clone());
+impl<'a, T: Row> From<&'a RelImpl<'a, T>> for Rel<'a, T> {
+    fn from(s: &'a RelImpl<'a, T>) -> Rel<'a, T> {
+        Rel(s)
+    }
+}
+
+pub struct DataRel<'a, T: Row> {
+    rel: &'a RelImpl<'a, T>,
+    vals: RefCell<HashMap<T, ()>>,
+}
+
+impl<'a, T: Row> DataRel<'a, T> {
+    pub fn add(&'a self, t: &T) {
+        let mut vals_mut = self.vals.borrow_mut();
+        let entry = vals_mut.entry(t.clone());
 
         match entry {
             Entry::Occupied(_) => (),
@@ -70,9 +106,9 @@ impl<T: Row> DataRel<T> {
         }
     }
 
-    pub fn remove(&mut self, t: &T) {
-        let entry = self.vals.entry(t.clone());
-
+    pub fn remove(&'a self, t: &T) {
+        let mut vals_mut = self.vals.borrow_mut();
+        let entry = vals_mut.entry(t.clone());
         match entry {
             Entry::Vacant(_) => (),
             Entry::Occupied(entry) => {
@@ -81,92 +117,97 @@ impl<T: Row> DataRel<T> {
             }
         }
     }
+
+    pub fn rel(&'a self) -> Rel<'a, T> {
+        self.rel.into()
+    }
 }
 
-pub fn data_rel<T: Row>(sys: System, initial: &[T]) -> DataRel<T> {
-    let result = Rel::impl_new(sys);
-    let mut this = DataRel {
-        rel: result.clone(),
-        vals: HashMap::new(),
-    };
+pub fn data_rel<'a, T: Row>(sys: &'a System<'a>, initial: &[T]) -> &'a DataRel<'a, T> {
+    let result = RelImpl::new(sys);
+    let this = sys.arena.alloc(DataRel {
+        rel: result,
+        vals: RefCell::new(HashMap::new()),
+    });
     for item in initial {
         this.add(item);
     }
     this
 }
 
-struct MemoRel<T: Row> {
-    rel: Rc<Rel<T>>,
-    vals: HashMap<T, i64>,
+struct MemoRel<'a, T: Row> {
+    rel: &'a RelImpl<'a, T>,
+    vals: RefCell<HashMap<T, i64>>,
 }
 
-impl<T: Row> Listener<T> for RefCell<MemoRel<T>> {
-    fn on_delta(&self, t: &T, delta: i64) {
+impl<'a, T: Row> Listener<'a, T> for MemoRel<'a, T> {
+    fn on_delta(&'a self, t: &T, delta: i64) {
         // TOOD: remove if zero
         assert!(delta != 0);
-        let mut self_mut = self.borrow_mut();
-        let current_count = self_mut.vals.entry(t.clone()).or_insert(0);
+        let mut vals_mut = self.vals.borrow_mut();
+        let current_count = vals_mut.entry(t.clone()).or_insert(0);
         let is_added = *current_count == 0;
         *current_count += delta;
         let is_removed = *current_count == 0;
         if is_added {
-            self_mut.rel.impl_delta(t, 1);
+            self.rel.impl_delta(t, 1);
         } else if is_removed {
-            self_mut.rel.impl_delta(t, -1);
+            self.rel.impl_delta(t, -1);
         }
     }
 }
 
-pub fn memo<T: Row>(sys: System, rel: Rc<Rel<T>>) -> Rc<Rel<T>> {
-    let result = Rel::impl_new(sys);
-    let this = MemoRel {
-        rel: result.clone(),
-        vals: HashMap::new(),
-    };
-    rel.add_listener(Box::new(RefCell::new(this)));
-    result
+pub fn memo<'a, T: Row>(sys: &'a System<'a>, rel: &'a Rel<'a, T>) -> Rel<'a, T> {
+    let result = RelImpl::new(sys);
+    let this = sys.arena.alloc(MemoRel {
+        rel: result,
+        vals: RefCell::new(HashMap::new()),
+    });
+    rel.add_listener(this);
+    result.into()
 }
 
-struct MapRel<T, R> {
-    rel: Rc<Rel<R>>,
-    f: Box<dyn Fn(&T) -> R>,
+struct MapRel<'a, T, R> {
+    rel: &'a RelImpl<'a, R>,
+    f: &'a dyn Fn(T) -> R,
 }
 
-impl<T, R> Listener<T> for Rc<MapRel<T, R>> {
-    fn on_delta(&self, t: &T, delta: i64) {
-        self.rel.impl_delta(&(self.f)(t), delta)
+impl<'a, T: Row, R: Row> Listener<'a, T> for MapRel<'a, T, R> {
+    fn on_delta(&'a self, t: &T, delta: i64) {
+        self.rel.impl_delta(&(self.f)(t.clone()), delta)
     }
 }
 
-pub fn map<T: Row, R: Row>(sys: System, rel: Rc<Rel<T>>, f: Box<dyn Fn(&T) -> R>) -> Rc<Rel<R>> {
-    let result = Rel::impl_new(sys);
-    let this = Rc::new(MapRel {
-        rel: result.clone(),
-        f: f,
-    });
-    rel.add_listener(Box::new(this));
-    result
+pub fn map<'a, T: Row, R: Row>(
+    sys: &'a System<'a>,
+    rel: Rel<'a, T>,
+    f: &'a dyn Fn(T) -> R,
+) -> Rel<'a, R> {
+    let result = RelImpl::new(sys);
+    let this = sys.arena.alloc(MapRel { rel: result, f });
+    rel.add_listener(this);
+    result.into()
 }
 
-struct JoinRel<A: Row, B: Row, K: Row> {
-    rel: Rc<Rel<(A, B)>>,
+struct JoinRel<'a, A: Row, B: Row, K: Row> {
+    rel: &'a RelImpl<'a, (A, B)>,
     key_a: Box<dyn Fn(&A) -> K>,
     key_b: Box<dyn Fn(&B) -> K>,
-    val_a: HashMap<K, HashMap<A, i64>>,
-    val_b: HashMap<K, HashMap<B, i64>>,
+    val_a: RefCell<HashMap<K, HashMap<A, i64>>>,
+    val_b: RefCell<HashMap<K, HashMap<B, i64>>>,
 }
 
-impl<A: Row, B: Row, K: Row> JoinRel<A, B, K> {
-    fn on_real_delta_a(&mut self, key: &K, t: &A, delta: i64) {
-        if let Some(other_list) = self.val_b.get(key) {
+impl<'a, A: Row, B: Row, K: Row> JoinRel<'a, A, B, K> {
+    fn on_real_delta_a(&'a self, key: &K, t: &A, delta: i64) {
+        if let Some(other_list) = self.val_b.borrow().get(key) {
             for (other, _) in other_list {
                 self.rel.impl_delta(&(t.clone(), other.clone()), delta);
             }
         }
     }
 
-    fn on_real_delta_b(&mut self, key: &K, t: &B, delta: i64) {
-        if let Some(other_list) = self.val_a.get(key) {
+    fn on_real_delta_b(&'a self, key: &K, t: &B, delta: i64) {
+        if let Some(other_list) = self.val_a.borrow().get(key) {
             for (other, _) in other_list {
                 self.rel.impl_delta(&(other.clone(), t.clone()), delta);
             }
@@ -174,17 +215,17 @@ impl<A: Row, B: Row, K: Row> JoinRel<A, B, K> {
     }
 }
 
-struct JoinRelA<A: Row, B: Row, K: Row>(Rc<RefCell<JoinRel<A, B, K>>>);
-struct JoinRelB<A: Row, B: Row, K: Row>(Rc<RefCell<JoinRel<A, B, K>>>);
+struct JoinRelA<'a, A: Row, B: Row, K: Row>(&'a JoinRel<'a, A, B, K>);
+struct JoinRelB<'a, A: Row, B: Row, K: Row>(&'a JoinRel<'a, A, B, K>);
 
-impl<A: Row, B: Row, K: Row> Listener<A> for JoinRelA<A, B, K> {
-    fn on_delta(&self, t: &A, delta: i64) {
+impl<'a, A: Row, B: Row, K: Row> Listener<'a, A> for JoinRelA<'a, A, B, K> {
+    fn on_delta(&'a self, t: &A, delta: i64) {
         assert!(delta != 0);
-        let mut rel = (*self.0).borrow_mut();
+        let rel = self.0;
         let key = (rel.key_a)(t);
         // todo: remove empty entries, clone()s are not needed in some cases
-        let current_count = rel
-            .val_a
+        let mut val_a = rel.val_a.borrow_mut();
+        let current_count = val_a
             .entry(key.clone())
             .or_insert(HashMap::new())
             .entry(t.clone())
@@ -200,14 +241,14 @@ impl<A: Row, B: Row, K: Row> Listener<A> for JoinRelA<A, B, K> {
     }
 }
 
-impl<A: Row, B: Row, K: Row> Listener<B> for JoinRelB<A, B, K> {
-    fn on_delta(&self, t: &B, delta: i64) {
+impl<'a, A: Row, B: Row, K: Row> Listener<'a, B> for JoinRelB<'a, A, B, K> {
+    fn on_delta(&'a self, t: &B, delta: i64) {
         assert!(delta != 0);
-        let mut rel = (*self.0).borrow_mut();
+        let rel = self.0;
         let key = (rel.key_b)(t);
         // todo: remove empty entries, clone()s are not needed in some cases
-        let current_count = rel
-            .val_b
+        let mut val_b = rel.val_b.borrow_mut();
+        let current_count = val_b
             .entry(key.clone())
             .or_insert(HashMap::new())
             .entry(t.clone())
@@ -223,22 +264,22 @@ impl<A: Row, B: Row, K: Row> Listener<B> for JoinRelB<A, B, K> {
     }
 }
 
-pub fn join<A: Row, B: Row, K: Row>(
-    sys: System,
-    rel_a: Rc<Rel<A>>,
-    rel_b: Rc<Rel<B>>,
+pub fn join<'a, A: Row, B: Row, K: Row>(
+    sys: &'a System<'a>,
+    rel_a: &'a Rel<'a, A>,
+    rel_b: &'a Rel<'a, B>,
     key_a: Box<dyn Fn(&A) -> K>,
     key_b: Box<dyn Fn(&B) -> K>,
-) -> Rc<Rel<(A, B)>> {
-    let rel = Rel::impl_new(sys);
-    let this = Rc::new(RefCell::new(JoinRel {
-        rel: rel.clone(),
+) -> Rel<'a, (A, B)> {
+    let rel = RelImpl::new(sys);
+    let this = sys.arena.alloc(JoinRel {
+        rel,
         key_a: key_a,
         key_b: key_b,
-        val_a: HashMap::new(),
-        val_b: HashMap::new(),
-    }));
-    rel_a.add_listener(Box::new(JoinRelA(this.clone())));
-    rel_b.add_listener(Box::new(JoinRelB(this.clone())));
-    rel
+        val_a: RefCell::new(HashMap::new()),
+        val_b: RefCell::new(HashMap::new()),
+    });
+    rel_a.add_listener(sys.arena.alloc(JoinRelA(this)));
+    rel_b.add_listener(sys.arena.alloc(JoinRelB(this)));
+    rel.into()
 }
--- src/simple_tests.rs
+++ src/simple_tests.rs
@@ -3,8 +3,11 @@ mod tests {
     use crate::simple::*;
 
     #[test]
-    fn exploration() {
-        let sys = System::new();
-        let _d1 = data_rel(sys, &[1, 2, 3]);
+    fn t1() {
+        with_system(|sys| {
+            let d1 = data_rel(&sys, &[1, 2, 3]);
+            let f = &|val| val + 1;
+            let d2 = map(sys, d1.rel(), sys.alloc(f));
+        })
     }
 }
